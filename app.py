7c95bbb2bc38304a7c642f63c104af8885ef0bab
Arif Aqeel <arifaqeelahmad059@gmail.com>
2025-10-16 03:09:25 +0500

    Basic Setup

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-#improt Flask class from "flask" module
-from flask import Flask
+# #improt Flask class from "flask" module
+# from flask import Flask
 
-#create object of Flask class
-app= Flask(__name__)
+# #create object of Flask class
+# app= Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<p> Hello Arif , Welcome to python development Journey</p>"
+# @app.route("/")
+# def home():
+#     return "<p> Hello Arif , Welcome to python development Journey</p>"
 
+# @app.route('/aboutUs/') #if we tried to access "/aboutUs", no error
+# def aboutUs():
+#     return "<p>this is about page </p>"
+
+# @app.route('/products') #if we tried to access '/products/', produce error
+# def products():
+#     return "<h1>PRODUCT PAGE</h1>" 
+
+###
+
+##this approch is not recommended , so we create the application factory
+###__init__.py is the application factory
---
+++ flaskr/app.py
@@ -0,0 +1,17 @@
+#improt Flask class from "flask" module
+from flask import Flask
+
+#create object of Flask class
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<p> Hello Arif , Welcome to python development Journey</p>"
+
+@app.route('/aboutUs/') #if we tried to access "/aboutUs", no error
+def aboutUs():
+    return "<p>this is about page </p>"
+
+@app.route('/products') #if we tried to access '/products/', produce error
+def products():
+    return "<h1>PRODUCT PAGE</h1>"
