616183fc7cedd2796962632159e0efe911e874dc
Arif Aqeel <arifaqeelahmad059@gmail.com>
2025-10-16 15:48:40 +0500

    Database Connection

---
+++ flaskr/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import os
+
+
+##application factory
+def create_app(test_config=None):
+    app=Flask(__name__, instance_relative_config=True) #configuration files are relative to the “instance” folder, not the main project folder.
+    #Set Default Configuration
+    app.config.from_mapping( #sets configuration values directly.
+        SECRETE_KEY='dev', #used by Flask for sessions and security. 'dev' is for development only
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'), #SQLite database file will be saved.
+    )
+    ##local configuration
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+        #It tries to load config.py from the instance folder (for example, instance/config.py).
+        #silent=True means Flask won’t crash if the file doesn’t exist.
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+    
+    #Ensure Instance Folder Exists
+    try:
+        os.makedirs(app.instance_path) #Tries to create the instance folder if it doesn’t already exist.your project will create the SQLite database file there.
+    except OSError:
+        pass
+
+    @app.route("/")
+    def home():
+        return "<h1> WELCOME TO THE HOME PAGE , THIS IS ARIF AQEEL AHMAD </h1>"
+
+    @app.route('/about')
+    def about():
+        return "<h3> about us page </h3>"
+    
+    return app
+
+
+
---
+++ flaskr/db.py
@@ -0,0 +1,30 @@
+import sqlite3
+from datetime import datetime
+import click
+from flask import current_app, g
+
+def get_db():
+    if 'db' not in g: #bd is just variable name
+        g.db= sqlite3.connect(
+            #database file path
+            current_app.config['DATABASE'], #gives access to the active Flask application instance.config -->is a dictionary containing all the app’s settings.DATABASE -->gets the value of the DATABASE key, which you defined in your factory
+            #type detection mode
+            detect_types=sqlite3.PARSE_DECLTYPES
+            #This is an optional argument to sqlite3.connect().
+            #It controls how SQLite converts data types between SQL and Python automatically.
+        )
+        g.db.row_factory=sqlite3.Row
+        #It tells SQLite to return rows as special objects that:
+        # Behave like both tuples and dictionaries.
+        # You can access values by column name
+        #without row factory , it will return the tuples
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    #If 'db' exists, it returns the database connection object and removes it from g.
+    # If 'db' doesn’t exist, it returns None (because of the second argument).
+    if db is not None:
+        db.close()
